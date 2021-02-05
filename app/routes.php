c16fd68095e01ad2382ce57d618c8e58ffda083e
DaisukeMatsuura <mail@brightful.jp>
2021-02-05 09:03:31 +0900

    ルーティング作成

--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Controllers\SampleController;
+use Controllers\BoardController;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -12,6 +13,15 @@ return function (App $app) {
         return $response;
     });
 
+    $app->add(function ($request, $handler) {
+        $response = $handler->handle($request);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    });
+
+    // 簡易受注管理アプリ
     $app->get('/', SampleController::class . ':index');
     $app->get('/add', SampleController::class . ':add');
     $app->post('/add', SampleController::class . ':create');
@@ -22,4 +32,13 @@ return function (App $app) {
     $app->get('/find', SampleController::class . ':find');
     $app->post('/find', SampleController::class . ':search');
     $app->get('/order', SampleController::class . ':order');
+
+    // Markdownコンテンツ投稿アプリ
+    $app->get('/board', BoardController::class . ':index');
+    $app->get('/board/login', BoardController::class . ':login');
+    $app->post('/board/login', BoardController::class . ':signin');
+    $app->get('/board/article/{id}', BoardController::class . ':article');
+    $app->post('/board/article/{id}', BoardController::class . ':post_comment');
+    $app->get('/board/user/{id}', BoardController::class . ':user');
+    $app->post('/board/user/{id}', BoardController::class . ':post_article');
 };
